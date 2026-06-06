9eefaabbe91d5f5dc60b1fc1c89eea4247616c13
Barkah Fadhil Sasmita <fadhilbarkah@gmail.com>
2026-06-06 11:20:48 +0700

    perf: reduce widget gzip size to <15KB

--- widget/vite.config.ts
+++ widget/vite.config.ts
@@ -19,8 +19,20 @@ export default defineConfig({
         extend: true,
       },
     },
+    sourcemap: true,
     cssCodeSplit: false,
-    minify: 'esbuild',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        passes: 3,
+        unsafe: true,
+      },
+      format: {
+        comments: false,
+      },
+    },
   },
   define: {
     'process.env.NODE_ENV': '"production"',
