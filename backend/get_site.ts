cbf9c4765cb9d7d77450a4558d4295c66220e9ce
Barkah Fadhil Sasmita <fadhilbarkah@gmail.com>
2026-06-06 20:25:29 +0700

    chore: remove throwaway scripts

--- backend/get_site.ts
+++
@@ -1,9 +0,0 @@
-import { db } from './src/db';
-import { sites } from './src/db/schema';
-import { eq } from 'drizzle-orm';
-
-async function run() {
-  const allSites = await db.select().from(sites).all();
-  console.log("All sites in DB:", allSites);
-}
-run();
--- backend/get_users.ts
+++
@@ -1,8 +0,0 @@
-import { db } from './src/db';
-import { users } from './src/db/schema';
-
-async function run() {
-  const allUsers = await db.select().from(users).all();
-  console.log("All users in DB:", allUsers.map(u => ({ id: u.id, email: u.email })));
-}
-run();
